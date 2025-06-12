42b810e333b697c0b0eea818ff131b44546b7445
TimofteRobert <123110591+TimofteRobert@users.noreply.github.com>
2025-06-12 18:08:25 +0300

    feat: add basic delete REST API for Category with response status code

--- src/main/java/com/ecommerce/ecom/service/CategoryServiceImplementation.java
+++ src/main/java/com/ecommerce/ecom/service/CategoryServiceImplementation.java
@@ -1,7 +1,9 @@
 package com.ecommerce.ecom.service;
 
 import com.ecommerce.ecom.model.Category;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +11,7 @@ import java.util.List;
 @Service
 public class CategoryServiceImplementation implements CategoryService {
     private List<Category> categories = new ArrayList<>();
+    private Long nextId = 1L;
 
     @Override
     public List<Category> getAllCategories() {
@@ -17,7 +20,16 @@ public class CategoryServiceImplementation implements CategoryService {
 
     @Override
     public void createCategory(Category category) {
+        category.setCategoryId(nextId++);
         categories.add(category);
     }
 
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categories.stream().filter(c -> c.getCategoryId().equals(categoryId))
+                .findFirst().orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+
+        categories.remove(category);
+        return "Category with categoryId: " + categoryId + " has been deleted.";
+    }
 }
